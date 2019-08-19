5ee945be818d7b2f9ed1848b5e03f351ba2f7bf4
asweenravi <asweenravi@gmail.com>
2019-08-19 10:07:52 +0530

    added get method

--- app/app.js
+++ app/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const routes = require('./routes')
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cors());
 
 routes(app);
-// app.get('/',(req,res)=>{
-//     res.send('got response');
-// })
+
 app.listen(3000, ()=>{
     console.log("app is listening to 5432")
 });
--- app/routes/recipe-routes/recipe.js
+++ app/routes/recipe-routes/recipe.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const pgp = require('pg-promise')();
+const cn = {
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres',
+    user: 'postgres',
+    password: 'Pass@123'
+};
+const db = pgp(cn);
 
-router.get('/recipes',(req, res)=>{
-    res.send('working');
-})
+router.get('/recipes', (req, res) => {
+    db.query('SELECT * FROM recipes').then((results) => {
+        res.status(200).send({ message: 'sucess', data: results });
+    }).catch(err => {
+        res.status(500).send({ message: 'database error', error: err });
+    })
+});
 
 module.exports = router;
